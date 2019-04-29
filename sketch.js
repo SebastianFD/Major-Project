515fe68258c16851b30500fd1ac3e7083c6756cd
SebastianFD <34584518+SebastianFD@users.noreply.github.com>
2019-04-29 15:20:05 -0600

    The Beginning.
    Start of the Major Project

--- sketch.js
+++ sketch.js
@@ -5,11 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let playerY, playerX;
+let iconSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+
+  iconSize = 20
+  playerX = width/2;
+  playerY = height/2;
 }
 
 function draw() {
   background(0);
+  keyPressed();
+  rect(playerX, playerY, iconSize, iconSize)
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    playerX -= 1
+  }
+  else if (keyCode === RIGHT_ARROW) {
+    playerX += 1
+  }
 }
